61cc96c200ca37d52c224aa96c06d24f3f5ae974
PKQ <zhutaonan@i-i.ai>
2020-06-17 16:23:21 +0800

    update

--- feature_experiment/test_acc.py
+++ feature_experiment/test_acc.py
@@ -1,28 +1,93 @@
 # -*- coding:utf-8 -*-
 # @author :adolf
 import os
+import random
+import numpy as np
 
-from pic_search.src.encoder.encode import Img2Vec
+# from pic_search.src.encoder.encode import Img2Vec
+from pic_search.src.encoder.encode_efficientent import Img2Vec
 from PIL import Image
 
 
 class MetricResult(object):
     def __init__(self):
-        self.feature_vec = Img2Vec()
+        self.feature_vec = Img2Vec(model_name="efficientnet-b0")
         self.transform = self.feature_vec.transform
 
-    def get_vec(self, img_path):
-        img = Image.open(img_path).convert("RGB")
+    def get_vec(self, img):
+        img = img.convert("RGB")
+        # img = Image.open(img_path).convert("RGB")
         img_tensor = self.transform(img)
-
+        img_tensor = img_tensor.unsqueeze(0)
         feature = self.feature_vec.feature_extraction(img_tensor)
 
         return feature
+
+    def get_smi(self, img1, img2):
+        # img1 = Image.open(path1)
+        # img2 = Image.open(path2)
+
+        vec1 = self.get_vec(img1)
+        vec2 = self.get_vec(img2)
+
+        dist = np.linalg.norm(vec1 - vec2)
+        return dist
+
+    def calculation_accuracy(self, ori_img, pos_img, neg_img):
+
+        pos_dist = self.get_smi(ori_img, pos_img)
+        neg_dist = self.get_smi(ori_img, neg_img)
+
+        if neg_dist > pos_dist:
+            return True
+        else:
+            return False
+
+    def get_acc(self, pos_cls_path, neg_cls_path):
+        pos_img_list = os.listdir(pos_cls_path)
+        neg_img_list = os.listdir(neg_cls_path)
+
+        pos_imgs = random.sample(pos_img_list, 2)
+        neg_imgs = random.choice(neg_img_list)
+
+        ori_path = os.path.join(pos_cls_path, pos_imgs[0])
+        pos_path = os.path.join(pos_cls_path, pos_imgs[1])
+        neg_path = os.path.join(neg_cls_path, neg_imgs)
+
+        ori_img = Image.open(ori_path)
+        pos_img = Image.open(pos_path)
+        neg_img = Image.open(neg_path)
+
+        return self.calculation_accuracy(ori_img, pos_img, neg_img)
+
+    def acc_metric(self, test_length=500):
+        img_root_path = 'baidu_pic/baidu_pic/'
+        cls_path_list = os.listdir(img_root_path)
+        # print(cls_path_list)
+        correct_result = 0
+        for i in range(test_length):
+            calculation_list = random.sample(cls_path_list, 2)
+            pos_cls_path = os.path.join(img_root_path, calculation_list[0])
+            neg_cls_path = os.path.join(img_root_path, calculation_list[1])
+            print('pos_cls_name', calculation_list[0])
+            print('neg_cls_name', calculation_list[1])
+            try:
+                if self.get_acc(pos_cls_path, neg_cls_path):
+                    correct_result += 1
+            except Exception as e:
+                print(e)
+                correct_result += 1
+
+        acc = correct_result / test_length * 1.0
+        print(acc)
+        return acc
 
 
 if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
-    print(MetricResult().get_vec("test_pic/1.png"))
-
-
+    # path1 = "test_pic/1.png"
+    # path2 = "test_pic/2.png"
+    # img1 = Image.open(path1)
+    # img2 = Image.open(path2)
+    MetricResult().acc_metric()
---
+++ pic_search/src/encoder/encode_resnet.py
@@ -0,0 +1,95 @@
+# -*- coding:utf-8 -*-
+# @author :adolf
+import torch
+import torchvision.models as models
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+import base64
+from PIL import Image
+from io import BytesIO
+
+from numpy import linalg as LA
+
+
+class Img2Vec(object):
+    def __init__(self):
+        self.TARGET_IMG_SIZE = 224
+        self.transform = transforms.Compose([
+            transforms.Resize((self.TARGET_IMG_SIZE, self.TARGET_IMG_SIZE)),
+            # transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = models.vgg16(pretrained=True).features
+        self.model.to(self.device)
+        self.model.eval()
+
+    @staticmethod
+    def base64_pil(base64_str):
+        image = base64.b64decode(base64_str)
+        image = BytesIO(image)
+        image = Image.open(image)
+        return image
+
+    def feature_extraction(self, img_tensor):
+        img_tensor = img_tensor.to(self.device)
+
+        with torch.no_grad():
+            feature_map = self.model(img_tensor)
+
+            feature_vector = F.max_pool2d(feature_map, kernel_size=feature_map.size()[-1])
+
+        feature_vector = torch.squeeze(feature_vector)
+        feature_vector = feature_vector.data.cpu().numpy()
+
+        return feature_vector
+
+    def __call__(self, base64list):
+        img_list = [self.base64_pil(base64img) for base64img in base64list]
+
+        img_list = [img.convert("RGB") for img in img_list]
+        img_list = [self.transform(img) for img in img_list]
+
+        img_tensor = torch.stack(img_list, dim=0)
+
+        feature_vector = self.feature_extraction(img_tensor)
+
+        return feature_vector
+
+        # norm_feat_list = list()
+
+        # for i in range(feature_vector.shape[0]):
+        #     norm_feat = feature_vector[i] / LA.norm(feature_vector[i])
+        #     norm_feat = [i.item() for i in norm_feat]
+        #     norm_feat_list.append(norm_feat)
+
+        # return norm_feat_list
+
+
+if __name__ == '__main__':
+    import os
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+
+    def path2base64(img_path):
+        with open(img_path, 'rb') as f:
+            image = f.read()
+            encodestr = str(base64.b64encode(image), 'utf-8')
+        return encodestr
+
+
+    img_path_1 = "test_pic/1.png"
+    img_path_2 = "test_pic/2.png"
+    img_path_3 = "test_pic/3.png"
+
+    img_list = list()
+    img_list.append(path2base64(img_path_1))
+    img_list.append(path2base64(img_path_2))
+    img_list.append(path2base64(img_path_3))
+
+    img_to_vec = Img2Vec()
+    norm_feat_list = img_to_vec(img_list)
+    print(norm_feat_list)
