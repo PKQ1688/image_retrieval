4d3f04a680d6b6b4e15650de9c38ced9a8c3225f
shiyu22 <cshiyu22@gmail.com>
2020-06-28 18:58:33 +0800

    update

--- pic_search/src/service/search.py
+++ pic_search/src/service/search.py
@@ -1,7 +1,7 @@
 import logging as log
 from common.config import DEFAULT_TABLE
-from indexer.index import milvus_client, search_vectors
-from indexer.tools import connect_mysql, search_by_milvus_ids, get_vector_by_ids
+from indexer.index import milvus_client, search_vectors, get_vector_by_ids
+from indexer.tools import connect_mysql, search_by_milvus_ids
 from encoder.encode import Img2Vec
 import time
 
@@ -15,8 +15,9 @@ def do_search(index_client, conn, cursor, img_to_vec, img_list):
     for ids in ids_milvus:
         vids = [x.id for x in ids]
 
-        status, vector = get_vector_by_ids(index_client, DEFAULT_TABLE, vids)
-        log.info(status, vector)
+        for ids in vids:
+            status, vector = get_vector_by_ids(index_client, DEFAULT_TABLE, ids)
+            log.info(status, vector)
         re_ids = search_by_milvus_ids(conn, cursor, vids)
         re_ids_img.append(re_ids)
     return re_ids_img
