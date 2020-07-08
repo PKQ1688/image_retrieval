684f2b7fcd37ac05f2e66dd2f3766b94b007edec
JackLCL <2415654524@qq.com>
2020-07-08 21:06:00 +0800

    update

--- pic_search/src/service/count.py
+++ pic_search/src/service/count.py
@@ -4,12 +4,12 @@ from common.config import DEFAULT_TABLE
 from indexer.index import milvus_client, count_table
 import eventlet
 
+eventlet.monkey_patch()
 
 def do_count(table_name):
     if not table_name:
     	table_name = DEFAULT_TABLE
-
-	eventlet.monkey_patch()
+	
     with eventlet.Timeout(3,False):
 	    print("doing count, table_name:", table_name)
 	    index_client = milvus_client()
