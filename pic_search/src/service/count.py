4e9429086cb6ae022d184ea8369345581b29081b
shiyu22 <cshiyu22@gmail.com>
2020-07-09 13:42:01 +0800

    add log

--- pic_search/src/service/count.py
+++ pic_search/src/service/count.py
@@ -8,7 +8,7 @@ def do_count(table_name):
     if not table_name:
         table_name = DEFAULT_TABLE
 
-    write_log("doing count, table_name:", table_name)
+    write_log("doing count, table_name:" + table_name)
     print("doing count, table_name:", table_name)
     index_client = milvus_client()
     num = count_table(index_client, table_name)
