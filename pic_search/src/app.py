ba60036443fd999ed8a9a8cb93ed626d9a86d327
PKQ <zhutaonan@i-i.ai>
2020-07-10 19:34:25 +0800

    update

--- pic_search/src/app.py
+++ pic_search/src/app.py
@@ -175,4 +175,4 @@ def do_search_images_api():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(threaded=True, host="0.0.0.0", port=5000)
--- taiji_experiment/test_mvilus_acc.py
+++ taiji_experiment/test_mvilus_acc.py
@@ -9,7 +9,7 @@ from feature_experiment.test_api import *
 img_file_path = "/home/shizai/datadisk5/cv/image_retrieval/taiji_test/"
 img_list = os.listdir(img_file_path)
 
-img_id = 21
+img_id = 6267
 img_name = img_list[img_id]
 # print(img_name)
 file_path = os.path.join(img_file_path, img_name)
@@ -28,7 +28,7 @@ result = result.split(',')[1:]
 flag = 0
 for rank_img_name in result:
     print(rank_img_name)
-    if "taiji_test" not in rank_img_name or flag > 15:
+    if "taiji_test" not in rank_img_name or flag > 10:
         continue
     img_index = rank_img_name.split('_')[-1]
     rank_img_path = os.path.join(img_file_path, img_list[int(img_index)])
