e49246c239901d682514046eadab8525b2d3ba8e
PKQ <zhutaonan@i-i.ai>
2020-07-27 10:12:28 +0800

    update

--- pic_search/src/app.py
+++ pic_search/src/app.py
@@ -189,4 +189,4 @@ def do_search_images_api():
 
 
 if __name__ == "__main__":
-    app.run(threaded=True, host="0.0.0.0", port=5000, debug=True)
+    app.run(threaded=True, host="0.0.0.0", port=5000, debug=False)
